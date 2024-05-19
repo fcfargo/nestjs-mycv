71c03d880a29103f8c7fee0dc3802c03a2cbdfe8
fcfargo <fcfargo90@gmail.com>
2024-05-19 16:59:59 +0900

    feat: add users controller function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-import { CreateUserDto } from './dtos/users.dto';
+import { CreateUserDto, UpdateUserDto } from './dtos/users.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -11,5 +11,25 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
     return this.userService.create(email, password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }

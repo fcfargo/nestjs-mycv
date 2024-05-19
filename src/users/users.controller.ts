a00a8c0a0b8b390c1fa24e4edff12f595ecceec3
fcfargo <fcfargo90@gmail.com>
2024-05-19 15:45:23 +0900

    feat: add  createUser controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { CreateUserDto } from './dtos/users.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    const { email, password } = body;
+    return this.userService.create(email, password);
   }
 }

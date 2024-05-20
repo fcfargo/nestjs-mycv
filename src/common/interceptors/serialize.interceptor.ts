a479a3bf2f5c0b8878df54f72e32c903498d4663
fcfargo <fcfargo90@gmail.com>
2024-05-20 20:37:52 +0900

    refactor: update serialize interceptor to process data into user response dto class instance

--- src/common/interceptors/serialize.interceptor.ts
+++ src/common/interceptors/serialize.interceptor.ts
@@ -1,22 +1,17 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { Observable, map } from 'rxjs';
+
+import { UserResponseDto } from '../../users/dtos/users.response.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // Run something before a request is handled
-    // by the request handler
-    console.log('before');
-    const code = context.switchToHttp().getResponse().statusCode;
     return next.handle().pipe(
-      map((data) => {
-        // Run something before the response is sent out
-        console.log('after');
-        return {
-          code,
-          message: 'success',
-          body: data,
-        };
-      }),
+      map((data: any) =>
+        plainToClass(UserResponseDto, data, {
+          excludeExtraneousValues: true,
+        }),
+      ),
     );
   }
 }

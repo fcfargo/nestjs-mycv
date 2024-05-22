3f3ffc6e7e792dc8bb2176fb18973301418dba5e
fcfargo <fcfargo90@gmail.com>
2024-05-22 16:25:58 +0900

    feat: add signup, signin function in auth service

--- src/common/interceptors/sucess.interceptor.ts
+++ src/common/interceptors/sucess.interceptor.ts
@@ -1,8 +1,7 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-@Injectable()
 export class SuccessInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const code = context.switchToHttp().getResponse().statusCode;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
+import { AuthService } from './auth.service';
 import { CreateUserDto, UpdateUserDto } from './dtos/users.dto';
 import { UserResponseDto } from './dtos/users.response.dto';
 import { UsersService } from './users.service';
@@ -8,12 +9,21 @@ import { Serialize } from '../common/interceptors/serialize.interceptor';
 @Controller('auth')
 @Serialize(UserResponseDto)
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
-    return this.usersService.create(email, password);
+    return this.authService.signup(email, password);
+  }
+
+  @Post('/signin')
+  signIn(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+    return this.authService.signin(email, password);
   }
 
   @Get('/:id')

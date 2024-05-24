cd1d92382b5812a1ea0944a5b71c60a297a976d3
fcfargo <fcfargo90@gmail.com>
2024-05-24 18:44:03 +0900

    fix: fix create reports logic to save user object

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 import { CreateReportDto } from './dtos/reports.dto';
 import { ReportsService } from './reports.service';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { AuthGuard } from '../common/guards/auth.guard';
+import { User } from '../models/users.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -10,7 +12,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,13 +4,16 @@ import { Repository } from 'typeorm';
 
 import { CreateReportDto } from './dtos/reports.dto';
 import { Report } from '../models/reports.entity';
+import { User } from '../models/users.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private readonly reportsRepository: Repository<Report>) {}
 
-  async create(requestData: CreateReportDto) {
+  async create(requestData: CreateReportDto, user: User) {
     const report = this.reportsRepository.create(requestData);
+
+    report.user = user;
 
     return this.reportsRepository.save(report);
   }

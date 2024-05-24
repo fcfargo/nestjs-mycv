268484a2b08e57b3a978567c5d3cb5cea3939be5
fcfargo <fcfargo90@gmail.com>
2024-05-24 19:29:53 +0900

    feat: add report response dto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 import { CreateReportDto } from './dtos/reports.dto';
+import { ReportResponseDto } from './dtos/reports.response.dto';
 import { ReportsService } from './reports.service';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
+import { Serialize } from '../common/decorators/serialize.decorator';
 import { AuthGuard } from '../common/guards/auth.guard';
 import { User } from '../models/users.entity';
 
 @Controller('reports')
+@Serialize(ReportResponseDto)
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 

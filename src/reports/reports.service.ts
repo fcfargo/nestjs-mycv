9491bb96518b153ab500606d7d4ee2ea3cf12efa
fcfargo <fcfargo90@gmail.com>
2024-05-26 15:17:38 +0900

    feat: add query builder logic to createEstimate service function

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,13 +2,33 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { ApproveReportDto, CreateReportDto } from './dtos/reports.dto';
+import { ApproveReportDto, CreateReportDto, GetEstimateDto } from './dtos/reports.dto';
 import { CurrentUserPayload } from '../common/interfaces/common.interface';
 import { Report } from '../models/reports.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private readonly reportsRepository: Repository<Report>) {}
+
+  createEstimate(requestData: GetEstimateDto) {
+    const { make, model, lng, lat, year, mileage } = requestData;
+    return (
+      this.reportsRepository
+        .createQueryBuilder()
+        .select('AVG(price)', 'price')
+        .where('make = :make', { make })
+        .andWhere('model = :model', { model })
+        .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+        .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+        .andWhere('year - :year BETWEEN -3 AND 3', { year })
+        .andWhere('approved IS TRUE')
+        // order by closest mileage
+        .orderBy('ABS(mileage - :mileage)', 'DESC')
+        .setParameters({ mileage })
+        .limit(3)
+        .getRawOne()
+    );
+  }
 
   async create(requestData: CreateReportDto, user: CurrentUserPayload) {
     const report = this.reportsRepository.create(requestData);
